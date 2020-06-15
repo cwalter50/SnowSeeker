07a989372a15688cf85f549de44b229115a691c9
Chris Walter <walt50@gmail.com>
2020-06-14 20:54:01 -0400

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Christopher Walter on 6/14/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
